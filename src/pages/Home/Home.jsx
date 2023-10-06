b55fa333d5a659fef4771f89b1c2ef5d1bb30f18
Kamilla <113370998+Kanin60@users.noreply.github.com>
2023-10-06 14:04:05 +0200

    update af style

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -38,21 +38,23 @@ export const Home = () => {
                 <p>Dyk ned i fortidens skatkammer af inspirerende kvinder, der har skabt forandring og efterladt deres uforglemmelige aftryk i historien. Fra dronninger og opdagelsesrejsende til kunstnere og videnskabskvinder - disse bemærkelsesværdige skikkelser har brudt grænser, udfordret normer og banet vejen for fremtidige generationer.</p>
                 <p>Vores blog vil tage dig med på en rejse gennem århundreder og kontinenter for at udforske kvindernes utrolige bedrifter, der har formet verden, som vi kender den i dag. Gå ikke glip af de utrolige historier om mod, intelligens og udholdenhed, der vil efterlade dig oplyst og inspireret.</p>
             </article>
-            {sortedPost?.slice(0, 2).map((item, index) => {
-                return (
-                    <article key={index}>
-                        <figure>
-                            <img src={item.image.url} alt={item.title} />
-                        </figure>
-                        <div className={style.text}>
-                            <h2 to="/">{item.title}</h2>
-                            <p style={{ fontSize: "12px", marginBottom: "1em" }}>{item.date}</p>
-                            <p>{item.summary}</p>
-                        </div>
-                    </article>
+            <div className={style.cardContainer}>
+                {sortedPost?.slice(0, 2).map((item, index) => {
+                    return (
+                        <article key={index}>
+                            <figure>
+                                <img src={item.image.url} alt={item.title} />
+                            </figure>
+                            <div className={style.text}>
+                                <h2 to="/">{item.title}</h2>
+                                <p style={{ fontSize: "12px", marginBottom: "1em" }}>{item.date}</p>
+                                <p>{item.summary}</p>
+                            </div>
+                        </article>
 
-                )
-            })}
+                    )
+                })}
+            </div>
         </section>
     )
 }
